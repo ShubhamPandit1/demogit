940893d653493385687ab169e5e9d5396c1d0480
ShubhamPandit1 <shubham.panditmit@gmail.com>
2020-10-12 11:53:39 +0530

    thtj

---
+++ shu/demo/Demo1.java
@@ -0,0 +1,28 @@
+package demo;
+
+public class Demo 
+{
+	float ar;
+    //float pi=(float) 3.14;
+	int area(int a,int b)
+	{
+		ar=a*b;
+		return (int) ar;
+	}
+	float area(int a,float pi)
+	{
+		ar=(float) (a*pi*a);
+		return ar;
+	}
+	float area(int a)
+	{
+		ar=a*a;
+		return ar;
+	}
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println();
+		System.out.println("shubham pandit");
+		
+	}
+}
